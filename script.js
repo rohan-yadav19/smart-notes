c06bee5473785e0c637c0dcef1206d58a46b034e
Rohan Yadav <yadavrohan98040@gmail.com>
2025-03-22 00:53:23 +0530

    add:localstorage to store notes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
-
+function shownotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+shownotes();
 function updateStorage() {
   localStorage.setItem("notes", notesContainer.innerHTML);
 }
@@ -17,5 +20,18 @@ notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
     updateStorage();
+  } else if (e.target.tagName == "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
   }
 });
